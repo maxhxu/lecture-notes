7975c54fe5860358a450b0c431992e97d34f735e
atentwo <atentwo@proton.me>
2025-09-16 14:28:22 -0400

    courses

--- grabfig.sh
+++ grabfig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# A shell script to pull figures from my phone to my computer
+
 adb pull "/sdcard/Figures/$(adb shell 'ls -t /sdcard/Figures/' | head -1 | tr -d '\r')" ~/lecture-notes/temp.jpg
 
 jpegtran -rotate 90 -outfile ~/lecture-notes/temp.jpg ~/lecture-notes/temp.jpg
